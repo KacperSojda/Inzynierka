13600e8eb8b8816cef962e68102b941efaf77e4b
Kacper Sojda <332630@uwr.edu.pl>
2025-08-03 15:35:58 +0200

    ChatBugFix

--- INZYNIERKA/obj/Debug/net8.0/INZYNIERKA.AssemblyInfo.cs
+++ INZYNIERKA/obj/Debug/net8.0/INZYNIERKA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INZYNIERKA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+375fb03e2e13eb64d969cb57189ad7aaa934dd21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b61286e278bb217ff7aa304e97a021595931f15b")]
 [assembly: System.Reflection.AssemblyProductAttribute("INZYNIERKA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INZYNIERKA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
