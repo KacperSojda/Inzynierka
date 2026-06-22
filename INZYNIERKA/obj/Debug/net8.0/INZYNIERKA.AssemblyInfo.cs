29cd885f2343a027a87d1df53b523a2da18a5290
kacpersojda05-collab <kacpersojda05@gmail.com>
2026-06-22 21:30:17 +0200

    Zaaktualizowano pakiet NuGet

--- INZYNIERKA/obj/Debug/net8.0/INZYNIERKA.AssemblyInfo.cs
+++ INZYNIERKA/obj/Debug/net8.0/INZYNIERKA.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INZYNIERKA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+449d9f33ba80f7a4f6fbe72a70634c0fc5444098")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c23cb05a08eb06e3f61a5390142be3e106b281bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("INZYNIERKA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INZYNIERKA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
